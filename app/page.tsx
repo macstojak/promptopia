8d315a58c4ab91fb64882957d02e9c090e0601f9
macstojak <maciej.stojak89@wp.pl>
2024-09-18 14:34:55 +0200

    vscode settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+      </h1>
+    </section>
   )
 }
 
